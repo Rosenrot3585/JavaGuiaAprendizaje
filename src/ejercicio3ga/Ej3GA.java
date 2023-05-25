67ad69bc46c3d5d7ba81b2ed0e7cd81834a6bca4
unknown <mcmp3585@gmail.com>
2023-05-25 17:32:48 -0300

    Convertidor de frase a Mayúsculas y Minúsculas

---
+++ src/ejercicio3ga/Ej3GA.java
@@ -0,0 +1,29 @@
+package ejercicio3ga;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Rosenrot3585
+ */
+
+// Program that takes a Sentence and print it in uppercases and then in
+// lowcases.
+
+public class Ej3GA {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Escriba una frase: ");
+        String frase = leer.nextLine();
+
+        System.out.println("La frase en mayúsculas es: ");
+        System.out.println(frase.toUpperCase());
+        System.out.println("");
+        System.out.println("La frase en minúsculas es: ");
+        System.out.println(frase.toLowerCase());
+        System.out.println("");
+    }
+
+}
